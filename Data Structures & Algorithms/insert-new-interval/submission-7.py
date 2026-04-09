6b92abad6ba61cb05b7a5b399c2dfb8e5e5ed12a
hpan0101 <hpan996@gmail.com>
2026-04-09 16:50:15 -0700

    Add: insert-new-interval - submission-7

---
+++ Data Structures & Algorithms/insert-new-interval/submission-7.py
@@ -0,0 +1,25 @@
+class Solution:
+    '''
+    while end of cur interval < new_start, add current interval to result list, index + 1
+    while old_start <= new_end, try merge the interval
+    add to result list
+    add the rest of the intervals to the result list
+    '''
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+        i = 0
+        n = len(intervals)
+        while i < n and intervals[i][1] < newInterval[0]:
+            res.append(intervals[i])
+            i += 1
+        
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+        res.append(newInterval)
+
+        for j in range(i, n):
+            res.append(intervals[j])
+        return res
+        
